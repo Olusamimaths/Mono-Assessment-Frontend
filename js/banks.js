edecb36f4d3af7d63ba59972dcd13f32ecfb8bc3
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2021-08-29 02:25:15 +0100

    :fire: Cleaning code base

--- js/banks.js
+++ js/banks.js
@@ -1,21 +1,48 @@
-window.onload = () => {
-  function getBankLists(url) {
-    fetch(url, {
-      method: "GET",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.status == 200) {
-          let content = "";
-          const { data } = res;
-          data.forEach((bank) => {
-            content += `      
+function mountAccordions() {
+  const accordions = document.getElementsByClassName("accordion");
+
+  for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      const panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+
+      for (let j = 0; j < accordions.length; j++) {
+        const currentAccordion = accordions[j];
+        if (currentAccordion != this) {
+          currentAccordion.classList.remove("active");
+          const currentPanel = currentAccordion.nextElementSibling;
+          currentPanel.style.maxHeight = null;
+        }
+      }
+    });
+  }
+}
+
+function createContent(res) {
+  if (res.status == 200) {
+    let content = "";
+    const { data } = res;
+    if (data.length == 0) {
+      document.querySelector(
+        ".banks"
+      ).innerHTML = `<p style="text-align: center; color: black;">Nothing to show</p>`;
+      return;
+    }
+    data.forEach((bank) => {
+      content += `      
             <div class="banks__content">
-                <div class="accordion">${bank.name}</div>
+            
+                <div class="accordion">
+                    <div class="banks_content__logo-name">
+                    <img src="${bank.imgUrl}" alt="" class="bank-logo">
+                    <p class="banks_content__bank-name">${bank.name}</p>
+                    </div>
+                </div>
                 <div class="panel">
                     <h4 class="panel__heading">
                         Floof will be able to access your:
@@ -30,13 +57,53 @@ window.onload = () => {
                 </div>
             </div>
         `;
-            document.querySelector(".banks").innerHTML = content;
-          });
-
-          console.log(res);
-        }
-      });
+      document.querySelector(".banks").innerHTML = content;
+      mountAccordions();
+    });
   }
+}
 
+function getBankLists(url) {
+  fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      createContent(res);
+    });
+}
+
+const searchBox = document.getElementById("searchBox");
+function search(url, bankName) {
+  fetch(`${url}/filter?key=${bankName}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      createContent(res);
+    });
+}
+
+searchBox.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  const searchText = document.getElementById("searchBox").value;
+  if (searchText != "") {
+    setTimeout(() => {
+      search("http://localhost:3000/banks", searchText);
+    }, 700);
+  } else {
+    getBankLists("http://localhost:3000/banks");
+  }
+});
+
+window.onload = () => {
   getBankLists("http://localhost:3000/banks");
 };
